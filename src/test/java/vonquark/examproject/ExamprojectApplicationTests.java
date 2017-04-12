42764975e227c2b511dbad0b79cfe007f5b42bcd
Kza von Quark <linus.lundahl@gmail.com>
2017-04-12 10:22:26 +0200

    Initial commit

---
+++ src/test/java/vonquark/examproject/ExamprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package vonquark.examproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExamprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
